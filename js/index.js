bcbc84986f504fcbbf230dcabb266793a2238082
Melvin Khan-Awa <melvinkhan-awa@Melvins-MacBook-Pro.local>
2019-05-14 12:46:35 +0100

    Add a load event listener on the window that displays a prompt

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+// mouseover event
 const funBusImg = document.querySelector(".intro img");
 
 funBusImg.addEventListener("mouseover", () => {
   funBusImg.classList.toggle("site-space-background");
+});
+
+// load event
+window.addEventListener("load", () => {
+  confirm(
+    "By using this site, you accept that we use cookies to improve on your experience on the site"
+  );
 });
