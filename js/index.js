0e58cb5a7b9973804917b873ef845f238750378d
Melvin Khan-Awa <melvinkhan-awa@Melvins-MacBook-Pro.local>
2019-05-14 15:48:14 +0100

    Uncomment load event

--- js/index.js
+++ js/index.js
@@ -41,16 +41,16 @@ funBusImg.addEventListener("mouseover", () => {
 });
 
 // load event
-// window.addEventListener("load", () => {
-//   const response = confirm(
-//     "By using this site, you accept that we use cookies to improve on your experience on the site"
-//   );
-//   if (response) {
-//     // code that uses cookies
-//   } else {
-//     // code that doesn't use cookies
-//   }
-// });
+window.addEventListener("load", () => {
+  const response = confirm(
+    "By using this site, you accept that we use cookies to improve on your experience on the site"
+  );
+  if (response) {
+    // code that uses cookies
+  } else {
+    // code that doesn't use cookies
+  }
+});
 
 // copy event
 [...paragraphs].forEach(p => {
